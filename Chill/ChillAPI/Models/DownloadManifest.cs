8a1d89983683330c93ff6684e73ae1d9dbb4f247
ChobbyCode <jacobhadfield99@gmail.com>
2024-02-01 18:33:41 +0000

    Models

---
+++ Chill/ChillAPI/Models/DownloadManifest.cs
@@ -0,0 +1,8 @@
+﻿
+namespace ChillAPI.Models;
+
+public class DownloadManifest
+{
+    public string Name { get; set; }
+    public string URI { get; set; }
+}
---
+++ Chill/ChillAPI/Models/DownloadManifestFile.cs
@@ -0,0 +1,7 @@
+﻿
+namespace ChillAPI.Models;
+
+public class DownloadManifestFile
+{
+    public List<DownloadManifest> files { get; set; } = new List<DownloadManifest>();
+}
